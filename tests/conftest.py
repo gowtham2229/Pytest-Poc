24a1dc33a1a372ed453e085913dfc3f9e1f88cf8
gowtham2229 <gowtham.t@paradigmit.com>
2026-03-05 17:31:29 +0530

    fixed the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,8 @@
 import sys
 import os
 import pytest_asyncio
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 
-# Add project root to Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app.main import app
@@ -11,5 +10,10 @@ from app.main import app
 
 @pytest_asyncio.fixture
 async def client():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(
+        transport=transport,
+        base_url="http://test"
+    ) as ac:
         yield ac
