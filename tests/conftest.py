d876ae3ddd1f68b1e49e5c967a98c1d4d08e4b40
gowtham2229 <gowtham.t@paradigmit.com>
2026-03-05 17:41:12 +0530

    fixed the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest_asyncio
-from httpx import AsyncClient
+from httpx import AsyncClient , ASGITransport
 from app.main import app
 from asgi_lifespan import LifespanManager
 
 @pytest_asyncio.fixture
 async def client():
+    transport = ASGITransport(app=app)
     async with LifespanManager(app):
         async with AsyncClient(app=app, base_url="http://test") as ac:
             yield ac
