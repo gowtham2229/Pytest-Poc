ee298b4fa6a98befb2fdf02a90334196716c809b
gowtham2229 <gowtham.t@paradigmit.com>
2026-03-05 17:16:40 +0530

    changed the main.py

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ async def home():
 
 
 
-# async def init_db():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
-# if __name__ == "__main__":
-#     asyncio.run(init_db())
-#     print("Database initialized successfully.")
+if __name__ == "__main__":
+    asyncio.run(init_db())
+    print("Database initialized successfully.")
 
 
 app.include_router(router=router, tags=["Tasks"])
